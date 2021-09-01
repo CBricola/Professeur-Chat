054ea607a0a882896632e0f219e0f80218305360
ClémentB <clement.bricola@hotmail.fr>
2021-09-01 15:14:50 +0200

    Initial version of professeur_chat generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/luracleb/professeurchat/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.luracleb.professeurchat.config;
---
+++ src/main/java/com/luracleb/professeurchat/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.luracleb.professeurchat.service.dto;
